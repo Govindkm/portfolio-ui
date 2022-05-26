2f26a9b58a0fd3b32c5a87c3157fb998d9378e53
Govind Mishra <govindkm.sv@gmail.com>
2022-05-26 10:15:27 +0530

    Nav Links changed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -12,7 +12,7 @@ function Nav() {
       <a href="#" className={activeSection === '#' ? 'active' : ''} onClick={() => { setActiveSection("#") }}><AiOutlineHome /></a>
       <a href="#about" className={activeSection === '#about' ? 'active' : ''} onClick={() => { setActiveSection("#about") }}><FiUser /></a>
       <a href="#experience" className={activeSection === '#experience' ? 'active' : ''} onClick={() => { setActiveSection("#experience") }}><BsGear /></a>
-      <a href="#services" className={activeSection === '#services' ? 'active' : ''} onClick={() => { setActiveSection("#services") }}><MdOutlineDesignServices /></a>
+      <a href="#portfolio" className={activeSection === '#portfolio' ? 'active' : ''} onClick={() => { setActiveSection("#portfolio") }}><MdOutlineDesignServices /></a>
       <a href="#contact" className={activeSection === '#contact' ? 'active' : ''} onClick={() => { setActiveSection("#contact") }}><AiOutlineContacts /></a>
     </nav>
   )
